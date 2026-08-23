7a68a475265b658a1691a25480ade9ffd891790f
夕鲤. <550664305@qq.com>
2026-08-23 13:55:40 +0800

    fix: 修改分数流转业务逻辑

--- student-score-system/src/main/java/com/student/studentscoresystem/service/impl/ScoreFlowServiceImpl.java
+++ student-score-system/src/main/java/com/student/studentscoresystem/service/impl/ScoreFlowServiceImpl.java
@@ -1,17 +1,14 @@
 package com.student.studentscoresystem.service.impl;
 
-
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.student.studentscoresystem.entity.ScoreFlow;
 import com.student.studentscoresystem.mapper.ScoreFlowMapper;
 import com.student.studentscoresystem.service.IScoreFlowService;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class ScoreFlowServiceImpl
         extends ServiceImpl<ScoreFlowMapper, ScoreFlow>
         implements IScoreFlowService {
-
 
 }
